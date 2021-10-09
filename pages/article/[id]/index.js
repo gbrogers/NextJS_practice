67376b9701a7bca8201a15cfe5d0bb1e6f01733a
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-10-09 14:30:52 -0600

    EOD

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,8 +1,28 @@
 import { useRouter } from "next/router";
-const article = () => {
+const article = ({ article }) => {
   // const router = useRouter();
   // const { id } = router.query;
-  return <div>This is an article {id}</div>;
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.id}</p>
+      <br />
+      <Link href="/">Go Back</Link>
+    </>
+  );
+};
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+  const article = await res.json();
+  return (props = {
+    article,
+  });
+};
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
 };
 
 export default article;
